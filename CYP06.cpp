1c7aec074d5bccc926af23f21199c6e35b3a220b
JosueChavez02 <josue-poncho@hotmail.com>
2024-11-25 12:43:45 -0600

    cambio2

--- CYP06.cpp
+++ CYP06.cpp
@@ -18,7 +18,7 @@
 	DICCIONARIO: Esta funcion crea el diccionario completo
 	char *	szNombre				:	Nombre del archivo de donde se sacaran las palabras del diccionario
 	char	szPalabras[][TAMTOKEN]	:	Arreglo con las palabras completas del diccionario
-	int		iEstadisticas[]			:	Arreglo con el numero de veces que aparecen las palabras en el diccionario
+
 
 **************************************/
 void Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[], int& iNumElementos)
